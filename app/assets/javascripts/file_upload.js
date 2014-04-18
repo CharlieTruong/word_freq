977a7e608b9c3246d03d0a00e849515268ca8be7
CharlieTruong <charlie.m.truong@gmail.com>
2014-04-18 00:00:31 -0500

    ajax file submission complete

--- app/assets/javascripts/file_upload.js
+++ app/assets/javascripts/file_upload.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
   $("#file_upload").submit(function(e){
     e.preventDefault();
+    $('#loader').show();
+    var data = new FormData();
+    data.append( 'file', $( '#file' )[0].files[0] );
+    var url = $("#file_upload").attr('action')
+    
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: data,
+      processData: false,
+      contentType: false,
+      success: function(response){
+        $('#loader').hide();
+        console.log(response);
+      },
+      dataType: 'json'
+    });    
   })
 });
+    
