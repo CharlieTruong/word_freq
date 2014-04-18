6ce0907baa6f948e3097792ee8f53e4a6f4d2c22
CharlieTruong <charlie.m.truong@gmail.com>
2014-04-18 12:50:48 -0500

    removed empty index method in controller

--- app/controllers/upload_files_controller.rb
+++ app/controllers/upload_files_controller.rb
@@ -1,10 +1,9 @@
 class UploadFilesController < ApplicationController
-  def index
-  end
 
   def word_count
     file = UploadFile.new(params['file'].tempfile)
     top25_words = file.word_count
     render json: top25_words
   end
+  
 end
